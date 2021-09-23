56f57ca434fe0b23360b23d9a1f50ec9a5efa5d5
young <brevinyoung@gmail.com>
2021-09-23 11:19:19 -0700

    added port for heroku

--- server.js
+++ server.js
@@ -4,32 +4,34 @@ const express = require("express");
 const path = require("path");
 const PORT = 3333;
 const fs = require("fs/promises");
+const PORT = process.env.PORT || 3001;
 
 const app = express();
-
+//adding middleware
 app.use(express.json());
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 //
-//
+// handles get request for the home page
 app.get("/", (req, res) => res.sendFile(path.join(__dirname, "index.html")));
 //
-//
+// handles get request for the note page
 app.get("/notes", (req, res) =>
   res.sendFile(path.join(__dirname, "public/notes.html"))
 );
 //
-//
+// gets any existing notes from the array
 app.get("/api/notes", (req, res) =>
   res.sendFile(path.join(__dirname, "db/db.json"))
 );
 //
-//
+// adds new notes into the array
 app.post("/api/notes", async (req, res) => {
+  //takes
   const dataToRead = await fs.readFile("./db/db.json", "utf8");
-
+  //
   const parsedData = JSON.parse(dataToRead);
-
+  // pushing
   parsedData.push({
     ...req.body,
     id: Math.floor((1 + Math.random()) * 0x10000)
@@ -40,7 +42,7 @@ app.post("/api/notes", async (req, res) => {
   const stringifiedData = JSON.stringify(parsedData);
 
   await fs.writeFile("./db/db.json", stringifiedData);
-  res.send("whatever");
+  res.send();
 });
 
 app.delete("/api/notes/:id", async (req, res) => {
@@ -53,7 +55,7 @@ app.delete("/api/notes/:id", async (req, res) => {
   const stringifiedData = JSON.stringify(notes);
 
   await fs.writeFile("./db/db.json", stringifiedData);
-  res.send("whatever");
+  res.send("bye");
 });
 //
 //
